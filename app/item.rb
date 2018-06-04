8d95c735489c7925c7199fedb7833dae9b87a5a0
github username <github email address>
2018-06-04 13:19:55 -0400

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,18 @@
 class Item
   attr_accessor :name, :price
 
+@@items=[]
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
   end
+
+def self.all
+@@items
 end
+
+end
+
+# Item.new("Figs",3.42)
+# Item.new("Pears",0.99)
